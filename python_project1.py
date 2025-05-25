5f41c872463197affc30bdfbee50a83fc3b1f2e8
Apiwat Maneechote <140755113+ApiwatManeechote@users.noreply.github.com>
2025-05-26 01:47:46 +0700

    Create python_project1.py
    try first python commit.

---
+++ python_project1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+  "id": [1, 2, 3, 4, 5],
+  "name": ["Goku", "Gohan", "Goten", "Vegeta", "Trunks"],
+  "sex": ["Male", "Male", "Male", "Male", "Male", ],
+  "age": [41, 21, 11, 46, 12]
+})
